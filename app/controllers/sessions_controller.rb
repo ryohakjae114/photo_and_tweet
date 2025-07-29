91471bc02f333a6468b18949b2fc090d7ee54d07
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2025-07-30 08:49:52 +0900

    ログイン後にログインに成功した旨のメッセージを表示する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:user_id, :password))
       start_new_session_for user
-      redirect_to after_authentication_url
+      redirect_to after_authentication_url, notice: "ログインしました"
     else
       redirect_to new_session_path, alert: "ログインに失敗しました"
     end
