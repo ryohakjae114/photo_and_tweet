622fb76ced065ce4694967681419343f70c92712
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2025-07-30 19:12:27 +0900

    ログインしてないとログイン画面以外遷移できないように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include Authentication
 
+  before_action :require_authentication
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
