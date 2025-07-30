b7b24d34d3f7f7f15f4815f6ad4778bc9e5bc0b1
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2025-07-30 18:59:20 +0900

    写真のエラーが出た時に、htmlでエラーを表示できなかった時もエラーメッセージを表示するように修正

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,11 @@ class Photo < ApplicationRecord
   has_one_attached :image
 
   validates :title, presence: true, length: { maximum: 30 }
+  validate :image_presence
+
+  private
+
+  def image_presence
+    errors.add(:image, 'をアップロードしてください') unless image.attached?
+  end
 end
