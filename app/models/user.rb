8fd0f4ab3952d4469662e26aae10903e5351694e
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2025-07-30 13:09:10 +0900

    photoモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :photos, dependent: :destroy
 
   validates :user_id, presence: true, uniqueness: true, length: { maximum: 20 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_29_224250) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_30_040542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -29,5 +37,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_29_224250) do
     t.index ["user_id"], name: "index_users_on_user_id", unique: true
   end
 
+  add_foreign_key "photos", "users"
   add_foreign_key "sessions", "users"
 end
