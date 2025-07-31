501c0b3959e3e5046c1d2a2cb200d15d4e79c7e2
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2025-07-31 19:40:09 +0900

    外部サービスに投稿できる

--- app/controllers/my/photos/tweets_controller.rb
+++ app/controllers/my/photos/tweets_controller.rb
@@ -1,2 +1,10 @@
 class My::Photos::TweetsController < ApplicationController
+  def create
+    @photo = Current.user.photos.find(params[:photo_id])
+    uri = URI('http://unifa-recruit-my-tweet-app.ap-northeast-1.elasticbeanstalk.com/api/tweets')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.request_post(uri.request_uri, URI.encode_www_form({ text: @photo.title, url: url_for(@photo.image) }),
+                      Authorization: "Bearer #{session[:access_token]}")
+    redirect_to root_path, notice: 'ツイートアプリに投稿しました'
+  end
 end
